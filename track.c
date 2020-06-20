33f9658dfb5c97d43b130ca997da729762dcd4d5
Matthias Wirth <matthias.wirth@gmail.com>
2020-06-20 10:00:35 +0200

    suppress some debug output when not debugging

--- track.c
+++ track.c
@@ -374,7 +374,7 @@ static int doGlobalCPR(struct aircraft *a, struct modesMessage *mm, double *lat,
                 fflag,
                 lat, lon);
 
-        if (receiver && a->seen_pos
+        if (Modes.debug_receiver && receiver && a->seen_pos
                 && *lat < 89
                 && *lat > -89
                 && (fabs(a->lat - *lat) > 35 || fabs(a->lon - *lon) > 35 || fabs(reflat - *lat) > 35 || fabs(reflon - *lon) > 35)
