d59198d6f56422f083c3ebdbb0077f22a84f14dd
Matthias Wirth <matthias.wirth@gmail.com>
2021-03-15 13:43:32 +0100

    testing shell script minor change

--- nogrind.sh
+++ nogrind.sh
@@ -4,6 +4,6 @@ rm -rf /run/test
 mkdir -p /run/test
 chown readsb /run/test
 source /etc/default/test
-#sudo -u readsb ./readsb $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test $@
-sudo -u readsb ./readsb $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test --net-connector 127.0.0.1,50006,beast_in $@
+sudo -u readsb ./readsb --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test $@
+#sudo -u readsb ./readsb --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test --net-connector 127.0.0.1,50006,beast_in $@
 
