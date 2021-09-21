82713b6655f58c4105e843fb504796afa82738c0
Matthias Wirth <matthias.wirth@gmail.com>
2021-09-21 15:41:38 +0200

    debug_bogus stuff

--- mode_s.c
+++ mode_s.c
@@ -2257,7 +2257,10 @@ void useModesMessage(struct modesMessage *mm) {
         Modes.synthetic_now = Modes.startup_time + mm->timestampMsg / 12000U;
     }
     if (Modes.debug_bogus) {
-        if (a && a->messages == 1 && a->registration[0] == 0 && !(a->addr & MODES_NON_ICAO_ADDRESS)) {
+        if (mm->addr != HEX_UNKNOWN && !(mm->addr & MODES_NON_ICAO_ADDRESS)) {
+            a = aircraftCreate(mm->addr);
+        }
+        if (a && a->messages == 1 && a->registration[0] == 0) {
             fprintf(stdout, "%6llx %5.1f not in DB: %06x\n",
                     (long long) mm->timestampMsg % 0x1000000,
                     10 * log10(mm->signalLevel),
