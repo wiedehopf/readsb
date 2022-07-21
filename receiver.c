8e0cbe47d548d387da883a145558c2dadf19e41f
Matthias Wirth <matthias.wirth@gmail.com>
2022-07-21 14:29:32 +0200

    bigger receiver table for viewadsb

--- receiver.c
+++ receiver.c
@@ -85,7 +85,7 @@ void receiverTimeout(int part, int nParts, int64_t now) {
     }
 }
 void receiverInit() {
-    if (Modes.netReceiverId || Modes.netIngest || Modes.debug_no_discard) {
+    if (Modes.netReceiverId || Modes.netIngest || Modes.debug_no_discard || Modes.viewadsb) {
         Modes.receiver_table_hash_bits = 16;
     } else {
         Modes.receiver_table_hash_bits = 8;
