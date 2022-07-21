0387042e44d2f316512e413dbdbd01d3ca2ff274
Matthias Wirth <matthias.wirth@gmail.com>
2022-07-21 21:50:01 +0200

    minor gpsOkLat

--- json_out.c
+++ json_out.c
@@ -731,10 +731,11 @@ char *sprintAircraftObject(char *p, char *end, struct aircraft *a, int64_t now, 
             }
         }
         if (a->nogpsCounter >= NOGPS_SHOW && now < a->seenAdsbReliable + NOGPS_DWELL && now > a->seenAdsbReliable + 15 * SECONDS) {
-            p = safe_snprintf(p, end, ",\"gpsOkBefore\":%.1f,\"gpsOkLat\":%f,\"gpsOkLon\":%f",
-                    a->seenAdsbReliable / 1000.0,
-                    a->seenAdsbLat,
-                    a->seenAdsbLon);
+            p = safe_snprintf(p, end, ",\"gpsOkBefore\":%.1f", a->seenAdsbReliable / 1000.0);
+            if (a->seenAdsbLat || a->seenAdsbLon) {
+                p = safe_snprintf(p, end, ",\"gpsOkLat\":%f,\"gpsOkLon\":%f", a->seenAdsbLat, a->seenAdsbLon);
+            }
+
         }
     }
 
