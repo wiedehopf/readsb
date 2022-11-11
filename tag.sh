dbe3947f6565572957a7da3c4cd5f05ad1028edc
Matthias Wirth <matthias.wirth@gmail.com>
2022-11-11 18:19:13 +0100

    update tag script

--- tag.sh
+++ tag.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
+
 VERSION="3.14.$(( $(cat version | cut -d'.' -f3) + 1 ))"
 echo "$VERSION" > version
 git add version
