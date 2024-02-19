b1d0309bc87a5977364c427c1f17583e8f602e8d
Matthias Wirth <matthias.wirth@gmail.com>
2024-02-19 22:19:02 +0100

    improve gdb script

--- gdb.sh
+++ gdb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-systemctl stop test
-rm -rf /run/test
-mkdir -p /run/test
-chown readsb /run/test
-source /etc/default/test
-cp -f readsb /tmp/test123
+instance=readsb
+systemctl stop ${instance}
+#rm -rf /run/${instance}
+mkdir -p /run/${instance}
+chown readsb /run/${instance}
+source /etc/default/${instance}
 
-gdb -batch -ex 'set confirm off' -ex 'handle SIGTERM nostop print pass' -ex 'handle SIGINT nostop print pass' -ex run -ex 'bt full' --args /tmp/test123 --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test $@
-#sudo -u readsb ./readsb --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/test --net-connector 127.0.0.1,50006,beast_in $@
+gdb -batch -ex 'set confirm off' -ex 'handle SIGTERM nostop print pass' -ex 'handle SIGINT nostop print pass' -ex run -ex 'bt full' --args /usr/bin/sudo -u readsb /usr/bin/readsb --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/${instance} $@
+#/usr/bin/sudo -u readsb /usr/bin/readsb --quiet $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS $JSON_OPTIONS --write-json /run/${instance} $@
 
